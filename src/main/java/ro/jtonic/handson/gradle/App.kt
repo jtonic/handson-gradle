74fe38bc5f04d82cd873a6aa50b8505dc302e004
jtonic <antonel.pazargic@gmail.com>
2017-10-29 23:15:09 +0200

    springboot configuration and a simple application

--- src/main/java/ro/jtonic/handson/gradle/App.kt
+++ src/main/java/ro/jtonic/handson/gradle/App.kt
@@ -1,16 +1,30 @@
 package ro.jtonic.handson.gradle
 
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
 /**
  * Created by Antonel Ernest Pazargic on 29/10/2017.
  * @author Antonel Ernest Pazargic
  */
+@SpringBootApplication
 class App {
 
     companion object {
 
         @JvmStatic
         fun main(vararg args: String) {
-            println("This is kotlin with gradle")
+            SpringApplication.run(App::class.java, *args)
         }
     }
+
+    @Bean
+    fun applicationRunner() = ApplicationRunner {
+        println("=" * 80)
+        println("The hello world spring boot application is running")
+        println("=" * 80)
+    }
 }
+
---
+++ src/main/java/ro/jtonic/handson/gradle/commons.kt
@@ -0,0 +1,8 @@
+package ro.jtonic.handson.gradle
+
+/**
+ * Created by Antonel Ernest Pazargic on 29/10/2017.
+ * @author Antonel Ernest Pazargic
+ */
+
+operator fun String.times(i: Int) = this.repeat(i)
