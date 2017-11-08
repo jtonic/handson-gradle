bc08b63dc3a997aa413283d3fc894d5a87818759
jtonic <antonel.pazargic@gmail.com>
2017-11-08 22:34:30 +0200

    Gradle 4.3.1 wrapper configuration. Simple rxjava with kt library.

---
+++ src/main/java/ro/jtonic/handson/gradle/rxjava/RxJavaMain.kt
@@ -0,0 +1,19 @@
+package ro.jtonic.handson.gradle.rxjava
+
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
+
+/**
+ * Created by Antonel Ernest Pazargic on 08/11/2017.
+ * @author Antonel Ernest Pazargic
+ */
+object RxJavaMain {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val source = listOf("A", "B", "C").toObservable()
+                .filter { it == "B" }
+                .subscribeBy(
+                        onNext = { println(it) }
+                )
+    }
+}
